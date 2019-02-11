284a677fd9c0b642dd8b7f06ead182282c85a336
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-02-11 11:48:55 +0600

    Chapter 13 ended

--- articles/models.py
+++ articles/models.py
@@ -17,4 +17,4 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('article-detail', args=[str(self.id)])
+        return reverse('article_detail', args=[str(self.id)])
